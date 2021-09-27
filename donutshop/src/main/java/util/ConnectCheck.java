0d9cbaba4097240042e98c5f02b53e12cde70b28
gou manabe <86933401+goumanabe@users.noreply.github.com>
2021-09-27 17:26:15 +0900

    donutsshop add
    ドーナッツショップのソースコード

---
+++ donutshop/src/main/java/util/ConnectCheck.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DonutDAO;
+
+/**
+ * Servlet implementation class ConnectCheck
+ */
+@WebServlet("/ConnectCheck")
+public class ConnectCheck extends HttpServlet {
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DonutDAO dao=new DonutDAO();
+		dao.ConnectCheck();
+	}
+}
